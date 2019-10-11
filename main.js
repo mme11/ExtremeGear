052adae4f938a51979d3f072933b29ae0d5b15f2
af116 <af116@hw.ac.uk>
2019-10-11 16:32:33 +0400

    race track

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ var car= new Image();
 var track= new Image();
 
 car.src = "images/car.png";
-track.src="images/track.png"
+track.src="images/newtrack.png";
 
 
 function draw()
 {
-    ctx.drawImage(track,0,0); 
+    ctx.drawImage(track,0,0);
+    ctx.drawImage(car,500,40,50,50);
     requestAnimationFrame(draw); 
 
 }
