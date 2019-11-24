8993df909ed102b8a042be7e39eb0ab656e9d261
af116 <af116@hw.ac.uk>
2019-11-25 01:26:39 +0400

    final changes

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const urlencodedParser = dataparser();
 
 let clients=0;  //number of users
 io.sockets.on('connection',function(socket){    //when a connection is made
+socket.id=Math.random();
 
 console.log("connected")
    socket.on('score',function(score,username){
-    db.query("UPDATE webProgTable SET SCORE=" + score + "WHERE USERNAME=" + username+ ";",function(err,result){
+    connection.query("UPDATE webProgTable SET SCORE=" + score + "WHERE USERNAME=" + username+ ";",function(err,result){
         if(err){
             console.log(err);
         }
@@ -22,18 +23,18 @@ console.log("connected")
     });
    });
 
-
+   
 
 });
 
-const db=mysql.createConnection({
+const connection=mysql.createConnection({
     host     :'localhost',
     user     :'root',
     password :'',
-    database :'nodemysql'
+    database :'users'
 });
 
-db.connect(function(err){   //connecting to the database
+connection.connect(function(err){   //connecting to the database
     if(err){
         throw err;
     }
@@ -41,8 +42,8 @@ db.connect(function(err){   //connecting to the database
 });
 
 app.get('/createdb',function(req,res){  //command which wa sused to create database
-    let sql='create database nodemysql';
-    db.query(sql,function(err,result){
+    let sql='create database users';
+    connection.query(sql,function(err,result){
         if(err){
             throw err;
         }
@@ -53,7 +54,7 @@ app.get('/createdb',function(req,res){  //command which wa sused to create datab
 
 app.get('/createtable',function(req,res){   //command which was used to create table
     let sql='CREATE TABLE webProgTable(id int AUTO_INCREMENT , username varchar(200) NOT NULL, password varchar(250) NOT NULL,score int NOT NULL, position int NOT NULL, PRIMARY KEY (id))';
-    db.query(sql,function(err,result){
+    connection.query(sql,function(err,result){
         if(err){
             throw err;
         }
@@ -62,64 +63,6 @@ app.get('/createtable',function(req,res){   //command which was used to create t
     });
 });
 
-/*app.get('/add1st',function(req,res){
-    let post={username:"hello",password:"user1",score:0,position:0};
-    let sql='INSERT INTO webprogtable SET ?';
-    let query= db.query(sql,post,function(err,result){
-        if(err){
-            throw err;
-        }
-    console.log(result);
-    res.send('pst1 added');
-    });
-});
-
-app.get('/add2nd',function(req,res){
-    let post={title:"post2" , body:"this is post2"};
-    let sql='INSERT INTO webprogtable SET ?';
-    let query= db.query(sql,post,function(err,result){
-        if(err){
-            throw err;
-        }
-    console.log(result);
-    res.send('pst2 added');
-    });
-});
-
-app.get('/selectall',function(req,res){
-    let sql='SELECT * FROM webprogtable';
-    db.query(sql,function(err,result){
-        if(err){
-            throw err;
-        }
-    console.log(result);
-    res.send("all selected");
-    });
-});
-
-app.get('/select1/:id',function(req,res){
-    let sql=`select * from webprogtable where id=${req.params.id}`;
-    db.query(sql,function(err,result){
-        if(err){
-            throw err;
-        }
-    console.log(result);
-    res.send('pst1 sleected');
-    });
-});
-
-app.get('/update/:id',function(req,res){
-    let newT="updated title";
-    let sql=`update webprogtable set title='${newT}' where id= ${req.params.id}`;
-
-    let query=db.query(sql,newT,function(err,result){
-        if(err){
-            throw err;
-        }
-    console.log(result);
-    res.send('pst1 title changed');
-    });
-});*/
 
 app.use('/client-side',express.static(__dirname+"/client-side"));
 
@@ -135,9 +78,9 @@ app.post('/auth',urlencodedParser, function (req,res) {  //when begin is clicked
     var password=req.body.password;
     clients++;
     console.log(username);
-    let post={username:username,password:password,score:0,position:0,Client:clients};
+    let post={username:username,password:password,score:0,position:0,clients:clients};
     let sql='INSERT INTO webprogtable SET ?';
-    let query= db.query(sql,post,function(err,result){
+    let query= connection.query(sql,post,function(err,result){
         if(err){
             throw err;
         }
@@ -158,8 +101,6 @@ http.listen(2020,function(err)  //puts the server at 2020
     console.log("Server at 2020");
 });
 
-module.exports = db;
-
-
+module.exports = connection;
 
 
--- client-side/main.js
+++ client-side/main.js
@@ -11,10 +11,9 @@ var boundary=40;
 var final=0;
 var socket=io();
 var cameraspeed=0;
-//var boundary_up=ypos+20;
+
 var dest_width,dest_height;
-//var trackx=-3700;
-//var tracky=-3700;
+
 var dx = 2;
 var dy = -2;
 var barx = cvs.width/2;
@@ -43,21 +42,21 @@ track.src="client-side/images/newtrack.png";
         socket.on('start',function(username){
             var username=username;
         });
-        myRaceArea.start();
+        Race.start();
     }   
 
-    var myRaceArea = { 
+    var Race = { 
 
     start:function()       //contains EVENT HANDLERS    
     {
-        this.interval = setInterval(updateRaceArea, 20);
+        this.interval = setInterval(updateArea, 20);
         document.addEventListener('keydown', function(e) {
             e.preventDefault();
-            myRaceArea.keys = (myRaceArea.keys || []);
-            myRaceArea.keys[e.keyCode] = (e.type == "keydown");
+            Race.keys = (Race.keys || []);
+            Race.keys[e.keyCode] = (e.type == "keydown");
         })
         document.addEventListener('keyup', function (e) {
-            myRaceArea.keys[e.keyCode] = (e.type == "keydown");
+            Race.keys[e.keyCode] = (e.type == "keydown");
             speed=0;
             cameraspeed=0;
             moveAngle=0;
@@ -65,103 +64,28 @@ track.src="client-side/images/newtrack.png";
         })
     },
 
-    stop : function() {
-        clearInterval(this.interval);
-    }
    
 }
-
-/*class Cars
-{
-    constructor()
-    {
-    this.speed=speed;
-    this.moveAngle=moveAngle;
-    this.width=50;
-    this.height=50;
-    this.x=xpos;
-    this.y=ypos;
-    this.angle=angle;
-    }
-
-    draw(){        //draws racetrack and car
-        this.updateRaceArea();
-       ctx.drawImage(track,-3700,-3700,10900,10900);
-        ctx.drawImage(RaceCar,this.x,this.y,this.width,this.height);
-        requestAnimationFrame(this.draw);
-        }
-
-
-        //clears area for the next sprite to be drawn and then assigns values to move angle or speed according to key press. It then calls newpos() and update()
-        updateRaceArea() {        
-            ctx.clearRect(this.x,this.y,this.width,this.height);
-            if(myRaceArea.keys)
-            {
-                if (myRaceArea.keys[37]) {this.moveAngle = 1; }
-                if (myRaceArea.keys[39]) {this.moveAngle = -1; }
-                if (myRaceArea.keys[38]) {this.speed= 1; }
-                if (myRaceArea.keys[40]) {this.speed= -1; }
-            }
-            
-            newPos();
-            update();
-        }
-
-
-         //updates position of sprite 
-         newPos(){
-
-           
-            this.angle += this.moveAngle * (Math.PI / 180) ;
-          
-            this.x += this.speed * Math.sin(this.angle);
-            this.y -= this.speed * Math.cos(this.angle);
-
-    
-           }
-        
-      
-        //saves current state of canvas and then draws the sprite in the updated position
-        update(){
-            ctx.save();
-            ctx.translate(this.x,this.y);
-            ctx.rotate(this.angle);
-            ctx.drawImage(RaceCar, this.width/-2 , this.height/-2 , this.width, this.width);
-    
-            ctx.restore(); 
-            }
-    
-}*/
-
-
-
-     function draw(){        //draws racetrack and car
-        updateRaceArea();
-       ctx.drawImage(track,room_xview,room_yview,room_sizex,room_sizey);//10900,10900
-        ctx.drawImage(RaceCar,xpos,ypos,carsize,carsize);
-        requestAnimationFrame(draw);
-        //updateRaceArea();
-        }
        
 
     //clears area for the next sprite to be drawn and then assigns values to move angle or speed according to key press. It then calls newpos() and update()
-      function updateRaceArea() {        
+      function updateArea() {        
         ctx.clearRect(xpos,ypos,50,50);
 
-        if(myRaceArea.keys)
+        if(Race.keys)
         {   
             
-            if (myRaceArea.keys[37]) 
+            if (Race.keys[37]) 
                 {moveAngle = 2; 
                 updateScore();}
-            if (myRaceArea.keys[39]) 
+            if (Race.keys[39]) 
                 {moveAngle = -2;
                 updateScore(); }
-            if (myRaceArea.keys[40]) 
+            if (Race.keys[40]) 
                 {cameraspeed= 10
                 updateScore();
                 speed= 5; }
-            if (myRaceArea.keys[38])
+            if (Race.keys[38])
                  {cameraspeed= -10;
                  updateScore();
                  speed= -5; }
@@ -198,37 +122,36 @@ track.src="client-side/images/newtrack.png";
         
             if (ypos > room_height-40) 
                 ypos = room_height-40;
-                -3490     -3650 
+            
             if(room_xview<-3490 && room_xview>-3650 && room_yview<-3100){
                 gameover();
             }
             
-            //xpos=xpos-room_xview/2;
-            //ypos=ypos-room_yview/2;
+
 
         }
         
       
-        //saves current state of canvas and then draws the sprite in the updated position
+        //moves the camera to move the car around the track
         function update(){
             
             dest_width=room_width;
             dest_height=room_height;
-            //ctx.drawImage(track,room_xview,room_yview,room_width,room_height,dest_xview,dest_yview,dest_width,dest_height);
+            
             ctx.drawImage(track,room_xview,room_yview,room_sizex,room_sizey);
             
             ctx.save();
-            //ctx.translate(xpos - cvs.width / 2, ypos - cvs.height / 2);
+        
             ctx.translate(xpos,ypos);
             ctx.rotate(angle);
-            //ctx.drawImage(track,-3700,-3700,10900,10900);
+            
             ctx.drawImage(RaceCar, -15 , -15 , carsize, carsize);
             
             ctx.restore(); 
             
             }
 
-            function updateScore(){
+            function updateScore(){//updates the score with the movement of the car
                 score_counter++;
                 if(score_counter%15==0){
                     score.text(parseInt(score.text())+1)
@@ -236,33 +159,8 @@ track.src="client-side/images/newtrack.png";
             } 
         }
 
-            /*function update(){
-                dest_width=room_width;
-                dest_height=room_height;
-    
-                if (xpos < 500) 
-                    xpos = 500; //500 is the starting point of the car 
-                
-                if (ypos< 40) 
-                    ypos = 40;//40 is the starting ypos of the car
-                
-                if (xpos > room_width-500) 
-                    xpos = room_width-500;
-                
-                if (ypos > room_height-40) 
-                    ypos = room_height-40;
-    
-                //ctx.drawImage(track,room_xview,room_yview,room_width,room_height,dest_xview,dest_yview,dest_width,dest_height);
-                ctx.drawImage(track,room_xview,room_yview,10900,10900);
-                ctx.save();
-                ctx.translate(xpos,ypos);
-                ctx.rotate(angle);
-                ctx.drawImage(RaceCar, -15 , -15 , 50, 50);
-               
-                ctx.restore(); 
-                }*/
+
 
                 function gameover(){
                     socket.emit('score',final,username);
                 }
-                
