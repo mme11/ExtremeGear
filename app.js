79749dd3c77d6398cd53ce25a7ebd6f7d6c0dcb8
mme11 <55647677+mme11@users.noreply.github.com>
2019-11-25 05:45:25 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ io.sockets.on('connection',function(socket){    //when a connection is made
 console.log("connected")
    socket.on('score',function(score,username){
     db.query("UPDATE webProgTable SET SCORE=" + score + "WHERE USERNAME=" + username+ ";",function(err,result){
-        if(err){
-            console.log(err);
-        }
-        else
+
             console.log(result);
     });
    });
@@ -34,18 +31,14 @@ const db=mysql.createConnection({
 });
 
 db.connect(function(err){   //connecting to the database
-    if(err){
-        throw err;
-    }
+   
     console.log("MySQL connected");     
 });
 
-app.get('/createdb',function(req,res){  //command which wa sused to create database
+app.get('/createdb',function(req,res){  //command which we used to create database
     let sql='create database nodemysql';
     db.query(sql,function(err,result){
-        if(err){
-            throw err;
-        }
+        
     console.log(result);
     res.send('database created');
     });
@@ -54,9 +47,7 @@ app.get('/createdb',function(req,res){  //command which wa sused to create datab
 app.get('/createtable',function(req,res){   //command which was used to create table
     let sql='CREATE TABLE webProgTable(id int AUTO_INCREMENT , username varchar(200) NOT NULL, password varchar(250) NOT NULL,score int NOT NULL, position int NOT NULL, PRIMARY KEY (id))';
     db.query(sql,function(err,result){
-        if(err){
-            throw err;
-        }
+        
     console.log(result);
     res.send('table created');
     });
@@ -137,24 +128,22 @@ app.post('/auth',urlencodedParser, function (req,res) {  //when begin is clicked
     console.log(username);
     let post={username:username,password:password,score:0,position:0,Client:clients};
     let sql='INSERT INTO webprogtable SET ?';
-    let query= db.query(sql,post,function(err,result){
-        if(err){
-            throw err;
-        }
+    let query= db.query(sql,post,function(result){
+        
+        
     
     console.log(result);
     console.log(clients)
             });
-    res.sendFile("C:/Users/Aysha Farheen/Documents/GitHub/furry-sadness/client-side/index.html");//file directing to
+    res.sendFile("C:/Users/Mohamed Elfarash/Desktop/ExtremeGear/client-side/index.html");//file directing to
                                                                                                  //main game
         });
 
 
 
 
-http.listen(2020,function(err)  //puts the server at 2020
-{   if(err)
-        throw err;
+http.listen(2020,function()  //puts the server at 2020
+{   
     console.log("Server at 2020");
 });
 
